dad67d5b41056a8f98366c403dd576b4d8248d9a
HPROGRAM <wjs2096@gmail.com>
2021-05-17 20:00:27 +0900

    생성: DTO

---
+++ src/dtos/create-user.dto.ts
@@ -0,0 +1,27 @@
+import { IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  user_id: string;
+
+  @IsString()
+  user_name: string;
+
+  @IsString()
+  user_password: string;
+
+  @IsString()
+  user_nickname: string;
+
+  @IsString()
+  user_job: string;
+
+  //   @IsString({default:null})
+  //   user_img: string;
+
+  //   @IsString()
+  //   profile_comment: string;
+
+  //   @IsString()
+  //   scrap_planer: string;
+}
