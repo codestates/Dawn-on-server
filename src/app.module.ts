e9823019bdd11279ff9c642a0870ed91523b6e68
HPROGRAM <wjs2096@gmail.com>
2021-05-17 20:07:58 +0900

    업데이트: app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/Users.entity';
 import { Posts } from './entities/Posts.entity';
 import { Stars } from './entities/Star_collections.entity';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { StarsModule } from './stars/stars.module';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
@@ -17,8 +22,11 @@ import { Stars } from './entities/Star_collections.entity';
       database: 'dawnon',
       entities: [Users, Posts, Stars], // 사용할 entity의 클래스명을 넣어둔다.
       synchronize: true,
-      autoLoadEntities: false, // false가 안전.
+      autoLoadEntities: true, // false가 안전.
     }),
+    UsersModule,
+    PostsModule,
+    StarsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
