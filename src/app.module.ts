0c53d49e1dc2115d157142e80ab9541f6fd5322c
gookgookJ <sksmsanj32@gmail.com>
2021-05-17 20:02:16 +0900

    업데이트 : src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,30 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Users } from './entities/Users.entity';
-import { Posts } from './entities/Posts.entity';
-import { Stars } from './entities/Star_collections.entity';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Users } from "./entities/Users.entity";
+import { Posts } from "./entities/Posts.entity";
+import { Stars } from "./entities/Star_collections.entity";
+import { UsersModule } from "./users/users.module";
+import { PostsModule } from "./posts/posts.module";
+import { StarsModule } from "./stars/stars.module";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
+      type: "mysql",
+      host: "localhost",
       port: 3306,
-      username: 'root',
-      password: '1234',
-      database: 'dawnon',
+      username: "root",
+      password: "gksrnr12",
+      database: "dawnon",
       entities: [Users, Posts, Stars], // 사용할 entity의 클래스명을 넣어둔다.
       synchronize: true,
       autoLoadEntities: false, // false가 안전.
     }),
+    UsersModule,
+    PostsModule,
+    StarsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
