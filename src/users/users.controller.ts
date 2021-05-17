fea9685f4b300d8cf68bd48c60a6aaaf404f5e34
HPROGRAM <wjs2096@gmail.com>
2021-05-17 20:02:12 +0900

    생성: users(module,controller,service)

---
+++ src/users/users.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Users } from '../entities/Users.entity';
+import { CreateUserDto } from '../dtos/create-user.dto';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { getConnection, Repository } from 'typeorm';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<any> {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll(): Promise<Users[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Users> {
+    return this.usersService.findOne(id);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,52 @@
+import { ForbiddenException, HttpStatus, Injectable } from '@nestjs/common';
+import { Users } from '../entities/Users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { getConnection, Repository } from 'typeorm';
+import { CreateUserDto } from '../dtos/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {}
+  //   private users: Users[] = []; // 데이터베이스 정보를 넣으면 됌.
+
+  async create(createUserDto): Promise<void> {
+    const isExist = await this.usersRepository.findOne({
+      user_id: createUserDto.user_id,
+    });
+    if (isExist) {
+      throw new ForbiddenException({
+        statusCode: HttpStatus.FORBIDDEN,
+        message: `이미 등록된 사용자입니다.`,
+        error: `상태코드:${HttpStatus.FORBIDDEN}`,
+      });
+    }
+    const { user_password, ...result } = await this.usersRepository.save(
+      createUserDto,
+    );
+    return result;
+  }
+
+  findAll(): Promise<Users[]> {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: string): Promise<Users> {
+    return this.usersRepository.findOne({ user_id: id });
+  }
+
+  async update(createUserDto): Promise<void> {
+    const newUsers = await this.usersRepository.findOne(createUserDto.id);
+    newUsers.user_nickname = createUserDto.user_nickname;
+    newUsers.user_img = createUserDto.user_img;
+    newUsers.user_job = createUserDto.user_job;
+    newUsers.user_password = createUserDto.user_password;
+    await this.usersRepository.save(newUsers);
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.usersRepository.delete(id);
+  }
+}
