a39201bfb9ff9149428144d83d46acdecda1ade6
HPROGRAM <wjs2096@gmail.com>
2021-05-18 18:33:22 +0900

    생성: guards관련 파일

---
+++ src/guards/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { jwtConstants } from '../contants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      user_id: payload.user_id,
+      user_nickname: payload.user_nickname,
+      user_job: payload.user_job,
+      user_name: payload.user_name,
+      user_img: payload.user_img,
+      profile_comment: payload.profile_comment,
+      scrap_planer: payload.scrap_planer,
+    };
+  }
+}
