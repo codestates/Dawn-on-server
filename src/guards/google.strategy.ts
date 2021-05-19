8aeef3702f1cf4ad003c92d9608a0a331dce8b93
HPROGRAM <wjs2096@gmail.com>
2021-05-19 19:41:24 +0900

    생성: google.guard

---
+++ src/guards/google.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private authService: AuthService) {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+      scope: ['profile', 'email'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { emails } = profile;
+    const user = {
+      user_id: emails[0].value,
+      accessToken,
+      refreshToken,
+    };
+    done(null, user);
+  }
+}
