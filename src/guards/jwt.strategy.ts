f984a1162a7bfcf735175e2208413634025b3b28
HPROGRAM <wjs2096@gmail.com>
2021-05-19 18:18:36 +0900

    업데이트: jwt.strategy

--- src/guards/jwt.strategy.ts
+++ src/guards/jwt.strategy.ts
@@ -1,27 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtConstants } from '../contants';
-
+import { TokenService } from 'src/auth/token.service';
+import { CreateUserDto } from 'src/dtos/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    private usersService: UsersService,
+    private tokenService: TokenService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
+      secretOrKey: 'apple',
     });
+    this.tokenService = tokenService;
   }
+  async validate(payload: any): Promise<CreateUserDto | null> {
+    const { user_id } = payload;
 
-  async validate(payload: any) {
-    return {
-      user_id: payload.user_id,
-      user_nickname: payload.user_nickname,
-      user_job: payload.user_job,
-      user_name: payload.user_name,
-      user_img: payload.user_img,
-      profile_comment: payload.profile_comment,
-      scrap_planer: payload.scrap_planer,
-    };
+    const user = await this.usersService.findOne(user_id);
+
+    if (!user) {
+      throw new UnauthorizedException('유효하지 않은 유저입니다.');
+    }
+
+    const accessTokenIat = this.tokenService.resolveAccessToken(
+      payload.accessToken,
+    );
+
+    if (accessTokenIat === null) {
+      throw new UnauthorizedException('유효하지 않은 토큰입니다.');
+    }
+
+    delete user.user_password;
+    return user;
   }
 }
