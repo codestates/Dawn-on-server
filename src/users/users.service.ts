63f772cb810dcaa5b332b965f2c353c87399568d
gookgookJ <sksmsanj32@gmail.com>
2021-05-19 18:29:08 +0900

    업데이트: user.service 수정

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,7 +13,8 @@ export class UsersService {
     private usersRepository: Repository<Users>
   ) {}
   //   private users: Users[] = []; // 데이터베이스 정보를 넣으면 됌.
-
+  // createUserDto.user.id = req.body.user_id (타입은 컨트롤러에서 명시하였음)
+  // 즉 DB안에 user_id와 req요청이 들어온 user_id가 일치하는것을 찾는 과정
   async create(createUserDto): Promise<any> {
     const isExist = await this.usersRepository.findOne({
       user_id: createUserDto.user_id,
