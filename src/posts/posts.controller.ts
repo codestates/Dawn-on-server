778dd69aa8e2f22b4b1694b16fa71285f7a758d8
gookgookJ <sksmsanj32@gmail.com>
2021-05-21 16:01:33 +0900

    업데이트: 체험하기 구현

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,36 +1,45 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
-import { CreateDataDto } from 'src/dtos/create-data.dto';
-import { Posts } from 'src/entities/Posts.entity';
-import { UsersService } from 'src/users/users.service';
-import { PostsService } from './posts.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from "@nestjs/common";
+import { CreateDataDto } from "src/dtos/create-data.dto";
+import { Posts } from "src/entities/Posts.entity";
+import { PostingAuthGuard } from "src/guards/posting-auth.guard";
+import { UsersService } from "src/users/users.service";
+import { PostsService } from "./posts.service";
 
-@Controller('posts')
+@Controller("posts")
 export class PostsController {
   constructor(
     private usersService: UsersService,
-    private postsService: PostsService,
+    private postsService: PostsService
   ) {
     this.usersService = usersService;
     this.postsService = postsService;
   }
-
-  @Post('posting')
-  async posting(@Body() body) {
+  @UseGuards(PostingAuthGuard)
+  @Post("posting")
+  async posting(@Req() req) {
     const newPost = await this.postsService.posting(
-      body.user_id,
-      body.postdatas,
+      req.postData.user_id,
+      req.postData.postdatas
     );
 
     return newPost;
   }
 
-  @Get('searchuser')
+  @Get("searchuser")
   async searchuser(@Body() body, @Res({ passthrough: true }) res) {
     const postings = await this.postsService.searchUser(body.user_id);
     return { user_id: body.user_id, postings: postings };
   }
 
-  @Get('searchall')
+  @Get("searchall")
   async searchall(@Body() body, @Res({ passthrough: true }) res) {
     const postingAll = await this.postsService.searchAll(body.user_job);
     return { user_job: body.user_job, postingAll: postingAll };
