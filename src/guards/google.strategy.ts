0021eff85bebe6aa2210b2715826a6144c4e288c
gookgookJ <sksmsanj32@gmail.com>
2021-05-25 09:53:12 +0900

    업데이트: 가드쓰지 않고 google oauth구현 시도

--- src/guards/google.strategy.ts
+++ src/guards/google.strategy.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-google-oauth20';
-import { AuthService } from '../auth/auth.service';
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-google-oauth20";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
   constructor(private authService: AuthService) {
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/callback',
-      scope: ['profile', 'email'],
+      clientID: `857821217387-1mg60omjr6uok183k4vrh5begv613glj.apps.googleusercontent.com`,
+      clientSecret: `process.env.GOOGLE_SECRET`,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+      scope: ["profile", "email"],
     });
   }
 
@@ -30,21 +30,22 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 
   async validate(
     accessToken: string,
-    refreshToken: string,
-    profile: any,
+    // refreshToken: string,
+    profile: any
   ): Promise<any> {
-    const { email, imggeUrl, name } = profile;
+    console.log("토큰 확인됨");
+    const { email, imageUrl, name } = profile;
     const userProfile = {
       user_id: email,
       accessToken,
-      refreshToken,
+      // refreshToken,
       user_nickname: name,
-      profileUrl: imggeUrl,
-      user_job: '전체',
+      profileUrl: imageUrl,
+      user_job: "전체",
     };
     const { user, tokens } = await this.authService.validateOAuthLogin(
       userProfile,
-      'google',
+      "google"
     );
     return { user, tokens };
   }
