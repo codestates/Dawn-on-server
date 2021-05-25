299f547344cc76f2f01098b20136c6c1f66bb878
gookgookJ <sksmsanj32@gmail.com>
2021-05-25 09:52:11 +0900

    업데이트: 가드쓰지 않고 google oauth구현 시도

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,15 @@
-import { ForbiddenException, HttpStatus, Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Users } from 'src/entities/Users.entity';
-import { Repository } from 'typeorm';
-import { CreateLoginDto } from '../dtos/create-login.dto';
-import { compare, hash } from 'bcrypt';
-import { CreateUserDto } from 'src/dtos/create-user.dto';
-import { UsersService } from 'src/users/users.service';
-import { TokenService } from './token.service';
-import { RefreshToken } from 'src/entities/RefreshToken.entity';
+import { ForbiddenException, HttpStatus, Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Users } from "src/entities/Users.entity";
+import { Repository } from "typeorm";
+import { CreateLoginDto } from "../dtos/create-login.dto";
+import { compare, hash } from "bcrypt";
+import { CreateUserDto } from "src/dtos/create-user.dto";
+import { UsersService } from "src/users/users.service";
+import { TokenService } from "./token.service";
+import { RefreshToken } from "src/entities/RefreshToken.entity";
+import axios from "axios";
 
 @Injectable()
 export class AuthService {
@@ -19,11 +20,12 @@ export class AuthService {
     private usersService: UsersService,
 
     @InjectRepository(RefreshToken)
-    private tokenService: TokenService,
+    private tokenService: TokenService
   ) {
     this.usersRepository = usersRepository;
     this.jwtService = jwtService;
     this.usersService = usersService;
+    this.tokenService = tokenService;
   }
 
   async validateUser(user_id: string, user_password: string): Promise<any> {
@@ -35,14 +37,14 @@ export class AuthService {
       throw new ForbiddenException({
         statusCode: HttpStatus.FORBIDDEN,
         message: [`등록되지 않은 사용자입니다.`],
-        error: 'Forbidden',
+        error: "Forbidden",
       });
     }
     //  console.log(user_password);
     //   console.log(user.user_password);
     let isMatch: boolean;
     if (await compare(user_password, user.user_password)) {
-      console.log('true');
+      console.log("true");
       isMatch = true;
     } else {
       // console.log("false");
@@ -56,7 +58,7 @@ export class AuthService {
       throw new ForbiddenException({
         statusCode: HttpStatus.FORBIDDEN,
         message: [`사용자 정보가 일치하지 않습니다.`],
-        error: 'Forbidden',
+        error: "Forbidden",
       });
     }
   }
@@ -100,4 +102,73 @@ export class AuthService {
     const refreshToken = await this.tokenService.generateRefreshToken(user);
     return { user, tokens: { accessToken, refreshToken } };
   }
+
+  async googleLogin(bodyData, res) {
+    if (bodyData.authorizationCode) {
+      await axios
+        .post("https://accounts.google.com/o/oauth2/token", {
+          client_id:
+            "857821217387-1mg60omjr6uok183k4vrh5begv613glj.apps.googleusercontent.com",
+          client_secret: "CD9hais_Oz92DYrhu2bTRTNa",
+          code: bodyData.authorizationCode,
+          redirect_uri: "http://localhost:3000/explore",
+          grant_type: "authorization_code",
+        })
+        .then((response) => {
+          console.log(response);
+          this.getGoogleInfo(
+            response.data.access_token,
+            response.data.token_type
+          );
+          /*     res.cookie({
+            //domain: "ibingo.link",
+            path: "/",
+            //httpOnly: true,
+            //secure: true,
+            sameSite: "none",
+          }); */
+          res.status(200).send({ accessToken: response.data.access_token });
+        })
+        .catch(() => res.status(403).send("No permission"));
+    } else {
+      res.status(404).send("Not Found Google authorizationCode");
+    }
+  }
+
+  // 구글에서 정보 받아오기
+  getGoogleInfo = async (accessToken: string, token_type: string) => {
+    await axios
+      .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then(async (res) => {
+        console.log(res);
+        let user: Users = await this.usersRepository.findOne({
+          user_id: res.data.name,
+        });
+
+        if (!user) {
+          const newUser = new Users();
+          newUser.user_id = res.data.name; //`${res.data.profileObj.email}[AUTH]`;
+          newUser.user_password = await hash(Math.random().toString(36), 10);
+          newUser.user_nickname = res.data.name;
+          newUser.user_job = "전체"; //res.data.profileObj.name; // 초기 닉네임은 사용자 이름
+          newUser.user_img = res.data.picture; //res.data.profileObj.imgUrl;
+          if (token_type) {
+            newUser.provider = "social";
+          } else {
+            newUser.provider = null;
+          }
+          user = await this.usersService.create(newUser);
+        } else {
+          const accessToken = await this.tokenService.generateAccessToken(user);
+          const refreshToken = await this.tokenService.generateRefreshToken(
+            user
+          );
+          return { user, tokens: { accessToken, refreshToken } };
+        }
+      });
+  };
 }
