6edd990f164d887bf4a53eb8bbc519b63e99a24b
gookgookJ <sksmsanj32@gmail.com>
2021-05-25 09:51:47 +0900

    업데이트: 가드쓰지 않고 google oauth구현 시도

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,43 +6,43 @@ import {
   Req,
   Res,
   UseGuards,
-} from '@nestjs/common';
-import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
-import { LocalAuthGuard } from 'src/guards/local-auth.guard';
-import { GoogleAuthGuard } from 'src/guards/google-auth.guard';
-import { UsersService } from 'src/users/users.service';
-import { AuthService } from './auth.service';
-import { TokenService } from './token.service';
-import { CreateUserDto } from 'src/dtos/create-user.dto';
+} from "@nestjs/common";
+import { JwtAuthGuard } from "src/guards/jwt-auth.guard";
+import { LocalAuthGuard } from "src/guards/local-auth.guard";
+import { GoogleAuthGuard } from "src/guards/google-auth.guard";
+import { UsersService } from "src/users/users.service";
+import { AuthService } from "./auth.service";
+import { TokenService } from "./token.service";
+import { CreateUserDto } from "src/dtos/create-user.dto";
 
-@Controller('auth')
+@Controller()
 export class AuthController {
   constructor(
     private authService: AuthService,
     private usersService: UsersService,
-    private tokenService: TokenService,
+    private tokenService: TokenService
   ) {
     this.authService = authService;
     this.usersService = usersService;
     this.tokenService = tokenService;
   }
 
-  @Post('singup')
+  @Post("singup")
   create(@Body() createUserDto: CreateUserDto): Promise<any> {
     return this.usersService.create(createUserDto);
   }
 
   @UseGuards(LocalAuthGuard)
-  @Post('signin')
+  @Post("signin")
   async signIn(@Req() req, @Res({ passthrough: true }) res): Promise<any> {
     const { user } = req;
 
     const accessToken = await this.tokenService.generateAccessToken(user);
     const refreshToken = await this.tokenService.generateRefreshToken(user);
 
-    res.cookie('refreshToken', refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       // domain: 'localhost:3000',
-      path: '/',
+      path: "/",
       // secure: true,
       httpOnly: true,
       // sameSite: 'None',
@@ -50,12 +50,12 @@ export class AuthController {
 
     return {
       data: { accessToken },
-      message: '로그인이 성공적으로 되었습니다.',
+      message: "로그인이 성공적으로 되었습니다.",
     };
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('mypage')
+  @Get("mypage")
   async getProfile(@Req() req): Promise<any> {
     const user = req.user;
 
@@ -63,40 +63,43 @@ export class AuthController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Post('signout')
+  @Post("signout")
   async signOut(@Req() req, @Res({ passthrough: true }) res): Promise<string> {
     const { user } = req;
-    res.clearCookie('refreshToken');
+    res.clearCookie("refreshToken");
     await this.tokenService.deleteRefreshTokenFromUser(user);
 
-    return '로그아웃 되었습니다.';
+    return "로그아웃 되었습니다.";
   }
 
-  @Get('google')
+  /*   @Get("google")
   @UseGuards(GoogleAuthGuard)
-  googleLogin() {
-    return;
-  }
+  googleLogin1() {}
 
-  @Get('google/redirect')
+  @Get("google/callback")
   @UseGuards(GoogleAuthGuard)
   async googleLoginCallback(
     @Req() req,
-    @Res({ passthrough: true }) res,
+    @Res({ passthrough: true }) res
   ): Promise<any> {
     const {
       // user,
       tokens: { accessToken, refreshToken },
     } = req.user;
-    res.cookie('refreshToken', refreshToken, {
-      domain: 'localhost:3000',
-      path: '/',
+    res.cookie("refreshToken", refreshToken, {
+      //domain: "localhost:3000",
+      path: "/",
       // secure: true,
       httpOnly: true,
       // sameSite: 'None',
     });
 
     // 메인화면 구성에 따라서 수정.
-    return res.redirect(`http://localhost:3000/oauth/?token=${accessToken}`);
+    return res.redirect(`http://localhost:3000/explore/?token=${accessToken}`);
+  } */
+
+  @Post("/googlelogin")
+  googleLogin(@Body() bodyData, @Res() res): Promise<any> {
+    return this.authService.googleLogin(bodyData, res);
   }
 }
