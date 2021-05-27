40c7e83518c590fafab2b986e7654ad10bcd4cfd
HPROGRAM <wjs2096@gmail.com>
2021-05-27 18:36:36 +0900

    업데이트: users 모듈

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,18 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Posts } from "src/entities/Posts.entity";
+import { RefreshToken } from "src/entities/RefreshToken.entity";
+import { Stars } from "src/entities/Star_collections.entity";
+import { Tags } from "src/entities/Tags.entity";
+import { Todos } from "src/entities/Todos.entity";
 import { Users } from "src/entities/Users.entity";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Users]),
+    TypeOrmModule.forFeature([Users, Posts, Stars, RefreshToken, Todos, Tags]),
     ConfigModule.forRoot({ envFilePath: ".env" }),
   ],
   controllers: [UsersController],
