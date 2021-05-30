fd30dd994827afe19c99cfe20843abeac475693a
gookgookJ <sksmsanj32@gmail.com>
2021-05-30 21:11:26 +0900

    업데이트: posts controller 반환 데이터이름 변경

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,9 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  Patch,
   Post,
   Req,
   Res,
@@ -18,7 +20,7 @@ import { PostsService } from "./posts.service";
 export class PostsController {
   constructor(
     private usersService: UsersService,
-    private postsService: PostsService,
+    private postsService: PostsService
   ) {
     this.usersService = usersService;
     this.postsService = postsService;
@@ -29,8 +31,8 @@ export class PostsController {
   async posting(@Req() req) {
     // console.log(req);
     const newPost = await this.postsService.posting(
-      req.body.user_id,
-      req.body.postdatas,
+      req.body.user_nickname,
+      req.body.postdatas
     );
 
     return newPost;
@@ -38,8 +40,8 @@ export class PostsController {
 
   @Post("search-user")
   async searchuser(@Body() body, @Res({ passthrough: true }) res) {
-    const postings = await this.postsService.searchUser(body.user_id);
-    return { user_id: body.user_id, postings: postings };
+    const postings = await this.postsService.searchUser(body.user_nickname);
+    return { user_nickname: body.user_nickname, postings: postings };
   }
 
   @Post("search-job")
@@ -77,19 +79,76 @@ export class PostsController {
   @Post("change-thumbsup")
   async change_thumbsup(@Body() body, @Res({ passthrough: true }) res) {
     const postDatas = await this.postsService.changeThumbsUp(
-      body.users_id,
-      body.posts_id,
+      body.user_PK,
+      body.post_PK
     );
 
     console.log(postDatas);
     res.status(200).send({ thumbs_up: postDatas });
   }
 
+  @Post("searchThumbsUp")
+  async searchThumbsUp(@Body() body, @Res({ passthrough: true }) res) {
+    const searchDatas = await this.postsService.searchThumbsUp(
+      body.user_PK,
+      body.post_PK
+    );
+    if (searchDatas === undefined) {
+      res.status(400).send("유효하지 않은 입력입니다.");
+    } else {
+      res.status(200).send(searchDatas);
+    }
+  }
+
+  @Post("myfeed")
+  async getPost(@Body() body, @Res({ passthrough: true }) res) {
+    const getPostingData = await this.postsService.getPost(body.user_PK);
+
+    if (getPostingData === false) {
+      res.status(404).send("유효한 유저가 아닙니다.");
+      return;
+    }
+    if (getPostingData.length === 0) {
+      res.status(400).send("포스팅 가져오기 실패: 작성된 포스팅이 없습니다.");
+    } else {
+      if (getPostingData !== undefined) {
+        res
+          .status(200)
+          .send({ message: "포스팅 가져오기 성공", postDatas: getPostingData });
+      }
+    }
+  }
+
+  @Delete("myfeed")
+  async deletePost(@Body() body, @Res({ passthrough: true }) res) {
+    const deletePostingData = await this.postsService.deletePost(body.post_PK);
+    console.log(deletePostingData);
+    if (deletePostingData === true) {
+      res.status(200).send("포스팅 삭제 성공");
+    } else {
+      res.status(400).send("포스팅 삭제 실패");
+    }
+  }
+
+  @Patch("myfeed")
+  async patchPost(@Body() body, @Res({ passthrough: true }) res) {
+    const patchPostingData = await this.postsService.pacthPost(
+      body.post_PK,
+      body.postdatas
+    );
+    console.log(patchPostingData);
+    if (patchPostingData === true) {
+      res.status(200).send("포스팅 업데이트 성공");
+    } else {
+      res.status(400).send("포스팅 업데이트 실패");
+    }
+  }
+
   // @Post('ranking')
   // async change_thumbsup(@Body() body, @Res({ passthrough: true }) res) {
   //   const postDatas = await this.postsService.changeThumbsUp(
-  //     body.users_id,
-  //     body.posts_id,
+  //     body.user_PK,
+  //     body.post_PK,
   //   );
 
   //   console.log(postDatas);
