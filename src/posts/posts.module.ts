d1044ded6ef9ad1747bf2710eaf48dda2a2cf93a
gookgookJ <sksmsanj32@gmail.com>
2021-05-31 17:45:11 +0900

    업데이트: module.ts providers 추가

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { TokenService } from "src/auth/token.service";
 import { jwtConstants } from "src/contants";
 import { Likes } from "src/entities/Likes.entity";
 import { Posts } from "src/entities/Posts.entity";
+import { RefreshToken } from "src/entities/RefreshToken.entity";
 import { Stars } from "src/entities/Star_collections.entity";
 import { Tags } from "src/entities/Tags.entity";
 import { Todos } from "src/entities/Todos.entity";
@@ -19,13 +21,21 @@ import { PostsService } from "./posts.service";
   imports: [
     ConfigModule.forRoot({ envFilePath: ".env" }),
     UsersModule,
-    // JwtModule.register({
-    //   secret: jwtConstants.secret,
-    //   signOptions: { expiresIn: '500s' },
-    // }),
-    TypeOrmModule.forFeature([Users, Posts, Stars, Todos, Tags, Likes]),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: "500s" },
+    }),
+    TypeOrmModule.forFeature([
+      Users,
+      Posts,
+      Stars,
+      Todos,
+      Tags,
+      Likes,
+      RefreshToken,
+    ]),
   ],
   controllers: [PostsController],
-  providers: [PostsService, StarsService, UsersService],
+  providers: [PostsService, StarsService, UsersService, TokenService],
 })
 export class PostsModule {}
